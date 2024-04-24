87a7e8808584b7a0b3bd863f3e6ff2f62cc78952
xusamson8 <xusamson14@gmail.com>
2024-04-24 16:08:35 -0700

    lab 5 python dfs commit

---
+++ Labs/Lab5/lab5.py
@@ -0,0 +1,20 @@
+class TreeNode(object):
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class lab5:
+    def isValidBST(self, root: TreeNode) -> bool :
+        def isValidBSTHelper(node, minBound, maxBound):
+            if not node:
+                return True #an empty BST is still a BST
+            if (node.val >= maxBound or node.val <= minBound):
+                #, if its greater than maxBound and lesser than minBound, it breaks the BST property
+                return False 
+                #return false since conditions aren't satisfied and node broke bst properties
+            #recursive call
+            return isValidBSTHelper(node.left, minBound, node.val) and isValidBSTHelper(node.right, node.val, maxBound) #if both are true then we can return true, whatever these return are the result
+            #when going left,we want to make sure left subtree is valid
+            # we update node with left child, its minBound stays the same since we're going left, the right boundary is the nodes value, the left subtree has to be less than the parent so its now the right boundary
+        return isValidBSTHelper(root, float("-inf"), float("inf")) #return the helper function we just created
