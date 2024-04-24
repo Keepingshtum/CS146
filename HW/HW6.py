8f42c5c7b46f3cb3f474f43b5b854e04c5c8915d
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-04-24 13:16:45 -0700

    Create HW6.py

---
+++ HW/HW6.py
@@ -0,0 +1,25 @@
+# Same as my Java solution, this solution uses double pointers, sorting, and checking for duplicates.
+
+class hw6:
+    def triplets(self, nums):
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
