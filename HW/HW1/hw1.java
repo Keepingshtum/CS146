ed29a983301e5b2bf17ed1340b2a5e13e9ad0baf
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-04-24 18:26:10 -0700

    [HW][HW1] - Java Optimized Solution - [Saket Gajavada]

--- HW/HW1/hw1.java
+++ HW/HW1/hw1.java
@@ -1,16 +1,29 @@
-package hw1;
+public static void main(String[] args) {
+	System.out.println(palindrome("nurses run"));
+}
 
-public class hw1 {
-    private static boolean palindrome(String s){
-        for (int i = 0; i < (int)(s.length()/2); i++){
-            if (s.charAt(i) != s.charAt(s.length() - i - 1)){
-                return false;
-            }
-        }
-        return true;
-    }
+public static boolean palindrome(String s) {
+	String lower = s.toLowerCase();
+	String newStr = "";
+	for (int i = 0; i < lower.length(); i++) {
+		if (Character.isLetterOrDigit(lower.charAt(i))) {
+			newStr += lower.charAt(i);
+		}
+	}
 
-    public static void main(String[] args) {
-        System.out.println(palindrome("bass bsab")?"palindrome":"not palindrome");
-    }
+	if (newStr.length() <= 1) {
+		return true;
+	}
+
+	int right = 0;
+	int left = newStr.length() - 1 - right;
+	while (right != left) {
+		if (newStr.charAt(right) != newStr.charAt(left)) {
+			return false;
+		}
+		right++;
+		left = newStr.length() - 1 - right;
+	}
+
+	return true;
 }
