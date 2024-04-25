308a7dd343a1625750e87f9b3928b1c8cfc036e9
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-04-24 23:12:07 -0700

    [HW][HW1] - Python Two Pointer Solution - [Saket Gajavada]

--- HW/HW1/hw1.py
+++ HW/HW1/hw1.py
@@ -1,8 +1,19 @@
-import math
 def palindrome(s):
-    for i in range(math.floor(len(s)/2)):
-        if s[i] != s[len(s) - i - 1]:
+    lower = s.lower()
+    newStr = ""
+    for i in lower:
+        if i.isdigit() or i.isalpha():
+            newStr += i
+    if len(newStr) <= 1:
+        return True
+    right = 0
+    left = len(newStr) - 1 - right
+    while right != left:
+        if newStr[right] != newStr[left]:
             return False
+        right += 1
+        left = len(newStr) - 1 - right
     return True
 
-print(palindrome("bass ssab"))
+if __name__ == "__main__":
+    print(palindrome("nurses run"))
