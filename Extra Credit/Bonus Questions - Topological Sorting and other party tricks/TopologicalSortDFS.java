be83f3232406be4918d3d1d7d432a5dc7ade42ae
Alx455 <158099631+Alx455@users.noreply.github.com>
2024-05-13 20:29:58 -0700

    Update TopologicalSortDFS.java

--- Extra Credit/Bonus Questions - Topological Sorting and other party tricks/TopologicalSortDFS.java
+++ Extra Credit/Bonus Questions - Topological Sorting and other party tricks/TopologicalSortDFS.java
@@ -34,6 +34,44 @@ public class Main {
     
 
     public static void main(String[] args) {
-        
+        List<List<Integer>> adjList = new ArrayList<List<Integer>>();
+
+
+
+        List<Integer> n0 = new ArrayList<Integer>();
+        n0.add(1);
+        n0.add(2);
+        adjList.add(n0);
+
+        List<Integer> n1 = new ArrayList<Integer>();
+        n1.add(3);
+        n1.add(4);
+        adjList.add(n1);
+
+        List<Integer> n2 = new ArrayList<Integer>();
+        n2.add(3);
+        n2.add(5);
+        adjList.add(n2);
+
+        List<Integer> n3 = new ArrayList<Integer>();
+        n3.add(4);
+        n3.add(5);
+        adjList.add(n3);
+
+        List<Integer> n4 = new ArrayList<Integer>();
+        adjList.add(n4);
+
+        List<Integer> n5 = new ArrayList<Integer>();
+        adjList.add(n5);
+
+
+        List<Integer> sortedGraph = topologicalSortDFS(adjList);
+        System.out.print("[");
+        for (int i = 0; i < sortedGraph.size(); i++) {
+            if (i == sortedGraph.size() - 1)
+                System.out.print(sortedGraph.get(i) + "]");
+            else
+                System.out.print(sortedGraph.get(i) + ", ");
+        }
     }
 }
